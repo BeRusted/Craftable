43ba7d38f7c1215ece15f6a087aa72cf0841d959
Mplan <mplan_@outlook.com>
2026-09-01 16:45:13 +0800

    docs: Add mod description

--- src/main/java/org/berusted/craftable/Craftable.java
+++ src/main/java/org/berusted/craftable/Craftable.java
@@ -1,8 +1,19 @@
 package org.berusted.craftable;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 public class Craftable implements ModInitializer {
+
+    public static final String MOD_ID = "craftable";
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public static Identifier id(String path) {
+        return Identifier.of(MOD_ID, path);
+    }
 
     @Override
     public void onInitialize() {
