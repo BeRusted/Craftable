2840037553d932dff238cef09437a540a3feeb14
Mplan <mplan_@outlook.com>
2026-09-01 15:37:15 +0800

    first commit

---
+++ src/client/java/org/berusted/craftable/client/CraftableClient.java
@@ -0,0 +1,10 @@
+package org.berusted.craftable.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CraftableClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/berusted/craftable/Craftable.java
@@ -0,0 +1,10 @@
+package org.berusted.craftable;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Craftable implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
